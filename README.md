55eb4757352df5d1c15f11428bd3a11c714c43c8
Nasruddinkhan <nasruddinkhan44@gmail.com>
2025-09-05 02:56:35 +0530

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # graphql-basic
 Graphql basic
+
+In grpahql it does not really care what you are having inside your spring controller.It loads things from the schema file.
+
+graphql schema mapped with controller via  @QueryMapping("hello")
+
+# Argument
+In graphql we are passing argument like sayHello(name : dataType): dataType in case if you want madatory
+add the  sayHello(name : dataType!)
+
--- src/main/java/com/nkhan/controller/HelloWorldController.java
+++ src/main/java/com/nkhan/controller/HelloWorldController.java
@@ -1,8 +1,11 @@
 package com.nkhan.controller;
 
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @Controller
 public class HelloWorldController {
@@ -11,4 +14,23 @@ public class HelloWorldController {
     public Mono<String> sayHello(){
         return Mono.just("Hello Graphql first program");
     }
+
+    @QueryMapping("hello")
+    public Mono<String> helloName(){
+        return Mono.just("Query mapping with alias");
+    }
+
+    @QueryMapping
+    public Mono<String> sayHelloTo(@Argument("name") String userName){
+        return Mono.fromSupplier(()-> """
+                Hello %s, Welcome to Graphql world
+                """.formatted(userName));
+    }
+
+    @QueryMapping
+    public Mono<Integer> randomId(){
+        return Mono.fromSupplier(()-> ThreadLocalRandom.current().nextInt(1, 1000));
+    }
+
+
 }
